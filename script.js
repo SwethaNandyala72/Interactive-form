4619a7530a581f3996e11354124d9e163a399d5f
swetha Nandyala <swethanandyala72@gmail.com>
2026-01-04 13:48:00 +0530

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById("contactForm").addEventListener("submit", function (e) {
     e.preventDefault();
 
-    // Clear old messages
+    
     document.getElementById("successMessage").textContent = "";
     document.querySelectorAll(".error").forEach(error => error.textContent = "");
 
@@ -12,14 +12,14 @@ document.getElementById("contactForm").addEventListener("submit", function (e) {
     const phone = document.getElementById("phone").value.trim();
     const message = document.getElementById("message").value.trim();
 
-    // Name validation
+    
     if (name.length < 3) {
         document.getElementById("nameError").textContent =
             "Name must be at least 3 characters";
         isValid = false;
     }
 
-    // Email validation
+    
     const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
     if (!email.match(emailPattern)) {
         document.getElementById("emailError").textContent =
@@ -27,24 +27,25 @@ document.getElementById("contactForm").addEventListener("submit", function (e) {
         isValid = false;
     }
 
-    // Phone validation
+    
     if (!/^\d{10}$/.test(phone)) {
         document.getElementById("phoneError").textContent =
             "Enter a valid 10-digit phone number";
         isValid = false;
     }
 
-    // Message validation
+    
     if (message === "") {
         document.getElementById("messageError").textContent =
             "Message cannot be empty";
         isValid = false;
     }
 
-    // Success message
+    
     if (isValid) {
         document.getElementById("successMessage").textContent =
             "Form submitted successfully!";
         document.getElementById("contactForm").reset();
     }
+
 });
